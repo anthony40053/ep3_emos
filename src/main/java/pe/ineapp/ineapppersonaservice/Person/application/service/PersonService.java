3b90ad9bc909e509361e561c52e49e796d17dfd9
DanPereyra <jsxcrash@gmail.com>
2022-11-13 00:24:16 -0500

    Project upload 10/11/22

---
+++ src/main/java/pe/ineapp/ineapppersonaservice/Person/application/service/PersonService.java
@@ -0,0 +1,21 @@
+package pe.ineapp.ineapppersonaservice.Person.application.service;
+
+import pe.ineapp.ineapppersonaservice.Person.domain.entity.Person;
+import pe.ineapp.ineapppersonaservice.Person.infrastructure.request.UserRequest;
+import pe.ineapp.ineapppersonaservice.Person.infrastructure.response.BasicResponse;
+import pe.ineapp.ineapppersonaservice.Person.infrastructure.response.UserResponse;
+
+import javax.persistence.Basic;
+import java.util.List;
+
+public interface PersonService {
+    public BasicResponse addUser(UserRequest request);
+
+    public UserResponse getAll();
+
+    public UserResponse getByDni(String dni);
+
+    public BasicResponse updateUser(UserRequest request, String dni);
+
+    public BasicResponse deleteUser(String dni);
+}
---
+++ src/main/java/pe/ineapp/ineapppersonaservice/Person/infrastructure/response/BasicResponse.java
@@ -0,0 +1,39 @@
+package pe.ineapp.ineapppersonaservice.Person.infrastructure.response;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class BasicResponse {
+    private int code;
+    private String mensaje;
+
+    public static BasicResponse whenSuccess() {
+        return BasicResponse.builder()
+                .code(200)
+                .mensaje("Success")
+                .build();
+    }
+
+    public static BasicResponse whenPassNotMatch() {
+        return BasicResponse.builder()
+                .code(500)
+                .mensaje("Incorrect password")
+                .build();
+    }
+
+    public static BasicResponse whenError(String message) {
+        return BasicResponse.builder()
+                .code(500)
+                .mensaje(message)
+                .build();
+    }
+
+    public static BasicResponse whenNoDataFound(String resource) {
+        return BasicResponse.builder()
+                .code(404)
+                .mensaje("No " + resource + " found")
+                .build();
+    }
+}
